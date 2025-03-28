87a453f3084ef8e5ba9317b167b2933891ab94b0
miguelsanchez <miguelsapem@gmail.com>
2025-03-28 11:00:02 +0100

    commit

--- Session 3-4/deck.py
+++ Session 3-4/deck.py
@@ -1,39 +1,47 @@
 import random
 
+
 class Card:
-    RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-    SUITS = ["♣", "♦", "♥", "♠"]
-    def __init__(self, suit, rank):
+    RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+    SUITS = ["♠", "♣", "♦", "♥"]
+
+    def __init__(self, rank, suit):
         if rank not in self.RANKS:
-            raise ValueError("Invalid rank")
+            raise ValueError(f"Invalid rank: {rank}")
         if suit not in self.SUITS:
-            raise ValueError("Invalid suit")
-        self._suit = suit
+            raise ValueError(f"Invalid suit: {suit}")
         self._rank = rank
+        self._suit = suit
 
     def __str__(self):
-        return f"{self._rank}{self._suit}"
+        return f"{self.rank}{self.suit}"
 
     def __repr__(self):
         return self.__str__()
 
     @property
-    def suit(self):
-        return self._suit
-
-    @property
     def rank(self):
         return self._rank
 
+    @property
+    def suit(self):
+        return self._suit
+
+
 class Deck:
     def __init__(self):
-        self._deck = []
-        for suit in Card.SUITS:
-            for rank in Card.RANKS:
-                self._deck.append(Card(suit, rank))
+        _cards = []
+        for rank in Card.RANKS:
+            for suit in Card.SUITS:
+                _cards.append(Card(rank, suit))
+        self._deck = _cards
+
+    @property
+    def deck(self):
+        return self._deck
 
     def __str__(self):
-        return str(self._deck)
+        return f"{self._deck}"
 
     def shuffle(self):
         random.shuffle(self._deck)
@@ -41,9 +49,9 @@ class Deck:
     def deal(self):
         return self._deck.pop(0)
 
+
 if __name__ == "__main__":
     deck = Deck()
     print(deck)
     deck.shuffle()
-    print(deck)
     print(deck.deal())
