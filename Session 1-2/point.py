780e8b67c6203c67f2f30ec854ffaca01337ff46
miguelsanchez <miguelsapem@gmail.com>
2025-03-28 10:58:12 +0100

    commit

--- Session 1-2/point.py
+++ Session 1-2/point.py
@@ -1,73 +1,72 @@
 import random
-from ftplib import parse227
+from math import *
 
 
 class Point:
     """
     Class modeling a real life 2D point
     """
+
     def __init__(self, x, y):
-        """
-        Initialize the point instance
-        :param x: the x axis coordinate value
-        :param y: the y axis coordinate value
-        """
         self.x = x
         self.y = y
 
     def __str__(self):
+        """
+        Magic method that defines how a point is printed
+        Returns:
+            string: A string representation of the point
+        """
         return f"<{self.x}, {self.y}>"
 
     def __repr__(self):
+        """
+        Magic method that defines how a point is represented
+        Returns:
+            string: A string representation of the point
+        """
         return self.__str__()
 
     def distance_orig(self):
-        return (self.x**2 + self.y**2)**0.5
+        return sqrt(self.x ** 2 + self.y ** 2)
 
     def __gt__(self, other):
-        """
-        Magic method that is called when you do self>other
-        :param other: the other point comparing against
-        :return: True/False
-        """
         return self.distance_orig() > other.distance_orig()
 
     def __eq__(self, other):
         return self.distance_orig() == other.distance_orig()
 
+
 if __name__ == "__main__":
+    p1 = Point(1, 2)
+    # p2 = Point(3, 4)
+    # p3 = Point("James", "Jane")
+    # print(p1.x, p1.y)
+    # print(p1)
+
     points = []
     for i in range(5):
         # create a random point
-        p = Point(
-            random.randint(-100, 100),
-            random.randint(-100, 100)
-        )
-        # append it to the list
+        p = Point(random.randint(-100, 100), random.randint(-100, 100))
         points.append(p)
 
-    print("unsorted points")
     print(points)
-    print("sorted points")
+    p = Point(12, 5)
+    print(p.distance_orig())
+
+    p1 = Point(2, 4)
+    p2 = Point(3, 3)
+
     points.sort()
     print(points)
 
-    found_equal = 0
+    found = 0
     count = 0
-    while True:
-        if found_equal == 10000:
-            break
-        p1 = Point(
-            random.randint(-100, 100),
-            random.randint(-100, 100)
-        )
-        p2 = Point(
-            random.randint(-100, 100),
-            random.randint(-100, 100)
-        )
-        count += 1
-        if p1 == p2:
-            # print(p1, p2)
-            found_equal += 1
 
-    print(f"probability is 1 in {count/found_equal} ")
+    while found < 1000:
+        count += 1
+        p1 = Point(random.randint(-100, 100), random.randint(-100, 100))
+        p2 = Point(random.randint(-100, 100), random.randint(-100, 100))
+        if p1 == p2:
+            found += 1
+    print(found / found, count / found)
